6e1c79ba242e77feed429e84bf1689c4f4232d3d
Seyed Shahabi <smfshahabi@gmail.com>
2020-02-04 23:27:41 +0100

    04/02/202004/02/202004/02/202004/02/2020

---
+++ basics_47.java
@@ -0,0 +1,14 @@
+public class basics_47{
+    public static void main(String[] args){
+        Object[] a = new Object[10];
+        a[0] = new Object();
+        a[1] = new String("durga");
+        a[2] = new Integer(10);
+
+        /*
+        in case of Object arrays we can make objects either from declared type of object of its child.
+        as you see here all objects are directly or indirectly child of array
+        look at video 8 to learn more
+        */
+    }
+}
---
+++ basics_48.java
@@ -0,0 +1,15 @@
+public class basics_48{
+    public static void main(String[] args){
+        Number[] n = new Number[10];
+        n[0] = new Integer(10);
+        n[1] = new Double(10.5);
+        // n[2] = new String("durga"); String is not a child of Number
+
+        /*
+        in case of Object arrays we can make objects either from declared type of object of its child.
+        as you see here String is not a child of Number
+         */
+
+
+    }
+}
