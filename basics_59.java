15d6736cc76e3e6471a90d0c7e504b422f9cd5c8
Seyed Shahabi <smfshahabi@gmail.com>
2020-02-08 15:30:48 +0100

    08/02/2020-3

---
+++ basics_59.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class basics_59{
+    static int x;
+    static double d;
+    static String s;
+    //static char ch;
+    static boolean b;
+    public static void main(String[] args){
+        System.out.println(x);
+        System.out.println(d);
+        System.out.println(s);
+        //System.out.println(ch);
+        System.out.println(b);
+        /*
+        as for instance variable we are not required to initialized the value for static variable as wel, jvm will provide the default value.
+         */
+    }
+
+}
